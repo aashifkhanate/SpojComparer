bfd8205847e26cef709cd90a66c4b408d531b52d
Aashif Khan <oaashifo@gmail.com>
2018-09-01 01:35:03 +0530

    Classize

--- compare/parsing.py
+++ compare/parsing.py
@@ -1,13 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-
 def foo(userA, userB):
-    user1 = "http://www.spoj.com/users/" + userA
-    user2 = "http://www.spoj.com/users/" + userB
-
-    list1 = doo(user1)
-    list2 = doo(user2)
+    url = "http://www.spoj.com/users/"
+    list1 = doo(url + userA)
+    list2 = doo(url + userB)
     listf = []
     for i in list2:
         flag = 0
@@ -20,33 +17,16 @@ def foo(userA, userB):
     return listf
 
 
-def moo(page):
-    url = urllib.request.urlopen(page)
-    s = url.read()
-    soup = BeautifulSoup(s)
-    harshit = soup.find('h2', class_="text-center").getText()
-    # print(harshit)
-    return harshit
-
-
 def doo(page):
     url = urllib.request.urlopen(page)
-
     s = url.read()
-
     soup = BeautifulSoup(s)
-
     all_tables = soup.find('table', class_="table table-condensed")
-
     lister = []
     if all_tables is not None:
         hello = all_tables.find_all('a')
         for hi in hello:
             temp = hi.getText()
             if temp is not '':
-                url_prob = "http://www.spoj.com/problems/" + temp + '/'
                 lister.append(temp)
-
     return lister
-
-# print (foo('aashifkhanate', 'yatind01'))
